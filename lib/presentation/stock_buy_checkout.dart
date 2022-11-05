d34e0707fa514e2a0190d0d900fb3c9fa046e1bb
Thiago de Sales <thiagobrunoms@gmail.com>
2022-11-05 11:55:08 -0300

    Create screen app bar

--- lib/presentation/stock_buy_checkout.dart
+++ lib/presentation/stock_buy_checkout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
+import 'package:stock_and_buy_app/presentation/widgets/default_container.dart';
 
 class StockBuyCheckout extends StatefulWidget {
   const StockBuyCheckout({super.key});
@@ -14,20 +15,62 @@ class _StockBuyCheckoutState extends State<StockBuyCheckout> {
   Widget build(BuildContext context) {
     return OrientationBuilder(
       builder: (context, orientation) {
-        print('Rebuilding... ${orientation.index}');
-        print('Rebuilding... ${orientation.name}');
 
         return Scaffold(
-          body: LayoutBuilder(
-            builder: (context, constraints) {
-              return Center(
-                child: Text('Center ${constraints.maxWidth}'),
-              );
-            }
+          body: SafeArea(
+            child: Row(
+              children: [
+                Flexible(
+                  flex: 2,
+                  child: _buildCollections()),
+                Flexible(
+                  flex: 1,
+                  child: _buildCart()),
+              ],
+            ),
           ),
           floatingActionButton: FloatingActionButton(onPressed: (){}, child: Icon(Icons.add),),
         );
       }
+    );
+  }
+  
+  Widget _buildCollections() {
+    return Column(
+      children: [
+        DefaultContainer(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(width: 1,),
+              const Text('All Collections'),
+              Icon(Icons.search, color: Theme.of(context).primaryColor)
+            ],
+          ),
+        )
+      ],
+    );
+  }
+  
+  Widget _buildCart() {
+    return Column(
+      children: [
+        DefaultContainer(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const SizedBox(width: 1,),
+              const Text('Cart'),
+              Row(
+                children: [
+                  Icon(Icons.delete, color: Theme.of(context).primaryColor),
+                  Icon(Icons.more_horiz, color: Theme.of(context).primaryColor)
+                ],
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
---
+++ lib/presentation/widgets/default_container.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DefaultContainer extends StatelessWidget {
+  final Widget child;
+  const DefaultContainer({super.key, required this.child});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      color: Colors.white,
+      child: child,
+    );
+  }
+}
